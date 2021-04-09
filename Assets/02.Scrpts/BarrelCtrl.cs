895ac9616cc2cba38e47c13a9bd17d670d28264d
unknown <kyulgon@naver.com>
2021-04-09 20:50:01 +0900

    Barrel 파티클, 사운드

--- Assets/02.Scrpts/BarrelCtrl.cs
+++ Assets/02.Scrpts/BarrelCtrl.cs
@@ -7,11 +7,15 @@ public class BarrelCtrl : MonoBehaviour
     public GameObject expEffect; // 폭발효과 프리팹을 저장할 변수
     public Mesh[] meshes; // 찌그러진 드럼통의 메쉬를 저장할 배열
     public Texture[] textures; // 드럼통의 텍스처를 저장할 배열
+    public float expRadius = 10.0f; // 폭발 반경
+    public AudioClip expSfx; // 폭팔음 오디오 클립 저장할 변수
 
     private int hitCount; // 총알이 맞은 횟수 저장
     private Rigidbody rb; // Rigidbody 컴포넌트 저장할 변수
     private MeshFilter meshFilter; // 외형을 변경하기 위한 MeshFilter
     private MeshRenderer _renderer; // 이미지 바꿀때 사용
+    private AudioSource _audio; // 오디오 저장
+
     
 
     private void Start()
@@ -19,6 +23,7 @@ public class BarrelCtrl : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         meshFilter = GetComponent<MeshFilter>();
         _renderer = GetComponent<MeshRenderer>();
+        _audio = GetComponent<AudioSource>();
 
         // 난수를 발생시켜 불규칙적인 텍스처를 적용
         _renderer.material.mainTexture = textures[Random.Range(0, textures.Length)];
@@ -41,8 +46,24 @@ public class BarrelCtrl : MonoBehaviour
         Destroy(effect, 2);
         rb.mass = 1.0f; // 무게를 가볍게 바꿈
         rb.AddForce(Vector3.up * 1000.0f); // 위로 솟구치는 힘을 가함
+        IndirectDamage(transform.position); // 폭발력 생성
 
-        int idx = Random.Range(0, meshes.Length); // meshs 갯수만큼 랜덤 수 뽑아냄
+        int idx = Random.Range(0, meshes.Length); // meshs 개수만큼 랜덤 수 뽑아냄
         meshFilter.sharedMesh = meshes[idx]; // 찌그러진 메쉬를 적용
+        GetComponent<MeshCollider>().sharedMesh = meshes[idx]; // 변경된 메쉬에 따라 수정
+
+        _audio.PlayOneShot(expSfx, 1.0f); // 폭발음 실행
+    }
+
+    private void IndirectDamage(Vector3 pos) // 폭발력을 주변에 전달하는 함수
+    {
+        Collider[] colls = Physics.OverlapSphere(pos, expRadius, 1 << 9); // 주변에 있는 드럼통을 모두 추출
+
+        foreach(var coll in colls)
+        {
+            var _rb = coll.GetComponent<Rigidbody>(); // 폭발 범위에 포함된 드럼통의 리지드바드 컴포넌트 추출
+            _rb.mass = 1.0f; // 드럼통의 무게를 가볍게 함
+            _rb.AddExplosionForce(1200.0f, pos, expRadius, 1000.0f); // 폭발력 전달 
+        }
     }
 }
--- Assets/02.Scrpts/FireCtrl.cs
+++ Assets/02.Scrpts/FireCtrl.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable] //총알 발사와 재장전 오디오 클립을 저장할 구조체
+public struct PlayerSfx
+{
+    public AudioClip[] fire;
+    public AudioClip[] reload;
+}
+
 public class FireCtrl : MonoBehaviour
 {
+    public enum WeaponType // 무기타입
+    {
+        RIFLE=0,
+        SHOTGUN
+    }
+
+    public WeaponType currWeapon = WeaponType.RIFLE; // 플레이어가 현재 들고 있는 무기를 저장할 변수
+
     public GameObject bullet; // 총알 프리팹
     public Transform firePos; // 총알 발사 좌표
     public ParticleSystem cartridge; // 탄피 추출 파티클
+    public PlayerSfx playerSfx; // 오디오 클립을 저장할 변수
+
     private ParticleSystem muzzleFlash; // 총구화염 파티클
+    private AudioSource _audio; // 오디오를 저장한 변수
 
     void Start()
     {
         muzzleFlash = firePos.GetComponentInChildren<ParticleSystem>(); // Children에 생성
+        _audio = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -27,5 +46,13 @@ public class FireCtrl : MonoBehaviour
         Instantiate(bullet, firePos.position, firePos.rotation); // Bullet 프리팹을 동적으로 생성
         cartridge.Play(); // 탄피 파티클 실행
         muzzleFlash.Play(); // 총구화염 파티클실행
+        FireSfx(); // 사운드 실행
+
+    }
+
+    private void FireSfx()
+    {
+        var _sfx = playerSfx.fire[(int)currWeapon]; // 현재 들고 있는 무기의 오디오 클립을 가져옴
+        _audio.PlayOneShot(_sfx, 1.0f); // 사운드 실행
     }
 }
