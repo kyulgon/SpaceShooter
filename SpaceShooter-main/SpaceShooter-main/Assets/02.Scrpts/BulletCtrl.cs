b61c81ecda5e0849d837d0413ea11c328ffdeb46
unknown <kyulgon@naver.com>
2021-04-28 23:15:35 +0900

    저번 Commit 재수정 업로드

---
+++ SpaceShooter-main/SpaceShooter-main/Assets/02.Scrpts/BulletCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    public float damage = 20.0f; // 총알의 파괴력
+    public float speed = 1000.0f; // 총알 발사 속도
+    
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed); // Z축으로 날아가기
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- UnityBook-master/README.md
+++
@@ -1,9 +0,0 @@
-
-<h2>절대강좌! 유니티 - 리소스 파일</h2>
-
-"절대강좌! 유니티" 책에서 사용하는 리소스 파일입니다.
-이 리소스는 개인 공부의 목적으로 만 사용해주세요.~
-
-Q&A는 [www.Unity3dStudy.com](http://www.unity3dstudy.com/books-qanda/) 에 글을 남겨주세요.
-
-![UnityBook2018 Cover](http://IndieGameMaker.github.io/images/books/unity2018_cover.png)
