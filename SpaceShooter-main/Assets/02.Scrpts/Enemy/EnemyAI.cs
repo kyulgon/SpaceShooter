8e9b7f54cd4cbe5ffed326a290c984cbb14f604c
kyulgon <57319096+kyulgon@users.noreply.github.com>
2021-05-01 18:38:42 +0900

    Enemy State 변경

---
+++ SpaceShooter-main/Assets/02.Scrpts/Enemy/EnemyAI.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public enum State // 적 캐릭터의 상태를 표현하기 위한 열거형 변수 정의
+    {
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    public State state = State.PATROL; // 기본 상태는 패트롤
+
+    private Transform playerTr; // 주인공의 위치를 저장할 변수
+    private Transform enemyTr; // 적 캐릭터의 위치를 저장할 변수
+
+    public float attackDist = 5.0f; // 공격 사정거리
+    public float traceDist = 10.0f; // 추적 사정거리
+
+    public bool isDie = false; // 사망 여부를 판단하 변수
+
+    private WaitForSeconds ws;
+
+    private void Awake()
+    {
+        //  주인공 게임오브젝트 추출
+        var player = GameObject.FindGameObjectWithTag("PLAYER");
+
+        if (player != null) // 주인공의 Transform 컴포넌트 추출
+            playerTr = player.GetComponent<Transform>();
+
+        enemyTr = GetComponent<Transform>(); // 적 캐릭터의 Transform 컴포넌트 추출
+
+        ws = new WaitForSeconds(0.3f);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(CheckState());
+    }
+
+    IEnumerator CheckState() // 적 캐릭터의 상태를 검사하는 코루틴
+    {
+        while(!isDie) // 적 캐릭터가 사망하지 전까지 도는 무한루프
+        {
+            if (state == State.DIE) // 상태가 사망이면 종류
+                yield break;
+
+            // 주인공과 적 캐릭터 간의 거리를 계산
+            float dist = (playerTr.position - enemyTr.position).sqrMagnitude;
+
+            if(dist <= attackDist * attackDist) // 공격 사정거리 이내인 경우
+            {
+                state = State.ATTACK;
+            }
+            else if(dist <= traceDist * traceDist) // 추적 사정거리 이내인 경우
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.PATROL;
+            }
+
+            yield return ws; // 0.3초동안 대기하는 동안 제어권을 양보
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ SpaceShooter-main/Assets/02.Scrpts/Enemy/MoveAgent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveAgent : MonoBehaviour
+{
+    public List<Transform> wayPoints; // 순찰 지점들을 저장하기 위한 List 타입 변수
+    public int nextIdx; // 다음 순찰 지점의 배열의 Index
+
+    private NavMeshAgent agent; // NavMeshAgent 컴포넌트를 저장할 변수
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>(); // NavMeshAgent 컴포넌트를 추출한 후 변수에 저장
+        agent.autoBraking = false; // 목적지에 가까워질수록 속도를 줄이는 옵션을 비활성화
+
+        var group = GameObject.Find("WayPointGroup"); // 하이러키 뷰의 WayPointGroup 게임오브젝트 찾기
+
+        if(group != null)
+        {
+            // WayPointGroup 하위에 있는 모든 Transform 컴포넌트를 추출한 후
+            // List 타입의 wayPoints 배열에 추가
+            group.GetComponentsInChildren<Transform>(wayPoints);
+
+            wayPoints.RemoveAt(0); // 배열의 첫 번째 항목 삭제
+        }
+
+        MoveWayPoint();
+    }
+
+    void Update()
+    {
+        // NavMeshAgent가 이동하고 있고 목적지에 도착했는지 여부를 계산
+        if(agent.velocity.sqrMagnitude >= 0.2f * 0.2f && agent.remainingDistance < 0.5f)
+        {
+            nextIdx = ++nextIdx % wayPoints.Count; // 다음 목적지의 배열 첨자를 계산
+            MoveWayPoint(); // 다음 목적지로 이동 명령을 수행
+        }
+    }
+
+    void MoveWayPoint()
+    {
+        if (agent.isPathStale) // 최단거리 경로 계산이 끝나지 않았으면 다음을 수행하지 않음
+            return;
+        
+        // 다음 목적지를 wayPoints 배열에서 추출한 위치로 다음 목적지를 지정
+        agent.destination = wayPoints[nextIdx].position;
+        // 내비게이션 기능을 활성화해서 이동을 시작함
+        agent.isStopped = false;
+    }
+}
